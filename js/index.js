9fb4d6c736eeede5822e659a70a8a3bae72da3e0
Praise Georgewill <georgewillpraise15@gmail.com>
2023-11-23 10:00:52 +0100

    pushing again

--- js/index.js
+++ js/index.js
@@ -11,7 +11,6 @@
 
 
 // the explore featur for the Charcter website
-
 document.querySelector('#character-btn').onclick = function(){
     window.open("http://127.0.0.1:5501/index.html");// you can put in a link where you want it to open at http://google.com
 };
